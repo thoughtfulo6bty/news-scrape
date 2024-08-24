7a805615f31ce7a90f3f0a47e37d06d440b4ec76
Murilo Simao <murilo.m.simao@gmail.com>
2024-08-24 02:08:46 -0300

    fix persistence layer to save results

--- src/adapters/persistence/excel_repository.py
+++ src/adapters/persistence/excel_repository.py
@@ -1,18 +1,26 @@
 from core.domain.interfaces import Respository
-from pandas import DataFrame
 from robocorp.tasks import get_output_dir
-from RPA.Excel.Files import Files
 from pathlib import Path
+from pandas import DataFrame
+from typing import List
+from core.domain.entities import NewsArticle
+from dataclasses import asdict
+import logging
 
 
 class ExcelRepository(Respository):
     def __init__(self) -> None:
         self.output_dir = get_output_dir()
-        self.output_path: Path = self.output_dir / 'file.csv'
-        self.excel = Files()
-    
-    def save(self, news_articles):
-        wb = self.excel.create_workbook()
-        wb.create_worksheet('Results')
-        self.excel.append_rows_to_worksheet(news_articles, header=True, name='Results')
-        wb.save(self.output_path)
+
+
+    def save(self, scrape_id: str, news_list: List[NewsArticle]):
+        filename = Path(f'news_scrape_result_{scrape_id}').with_suffix('.xlsx')
+        output_path: Path = self.output_dir / f'results_{scrape_id}' / filename
+
+        news_list_dict = list(map(asdict, news_list))
+        df = DataFrame(news_list_dict)
+        df.to_excel(output_path, 
+                    sheet_name=f'results_{scrape_id}', 
+                    index=False)
+        logging.info(f'Results Saved in: {output_path}')
+        
