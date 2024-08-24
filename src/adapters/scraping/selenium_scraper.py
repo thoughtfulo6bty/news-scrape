0e5e5586b30606252441cc522f1c2e0d206b97ba
Murilo Simao <murilo.m.simao@gmail.com>
2024-08-24 02:09:48 -0300

    add undetected chrome support to bypass any captcha

--- src/adapters/scraping/selenium_scraper.py
+++ src/adapters/scraping/selenium_scraper.py
@@ -1,26 +1,30 @@
 import logging
 from enum import Enum
-from pathlib import Path
-from typing import List, Literal, Optional
+from typing import List, Optional
 from uuid import uuid4
 from datetime import date, datetime
 
 from robocorp.tasks import get_output_dir
 from RPA.Browser.Selenium import Selenium
+from RPA.Browser.Selenium import ElementNotFound
 from RPA.HTTP import HTTP
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from datetime import timedelta
+
+import undetected_chromedriver as uc
 
 from core.domain.entities import NewsArticle
 from core.domain.interfaces import Scraper
 
-logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 
 class Elements(Enum):
-    SEARCH_TITLE = "h1[data-testid='Heading']"
+    SEARCH_TITLE = '//*[@id="main-content"]'
     ALL_OFFSET = 'xpath:/html/body/div[1]/div[2]/div[2]/div/div[2]/div[3]/span'
     NEWS_LIST = 'ul.search-results__list__2SxSK > li'
     NEWS_TITLE = "a[data-testid='Title']"
@@ -35,33 +39,47 @@ class SeleniumScraper(Scraper):
         self.offset = 'offset={}'
         self.base_url = 'https://www.reuters.com/site-search/?query={}&section={}&{}&date=any_time&sort=newest'
         self.browser = Selenium()
-        self.http = HTTP()
+        self._driver = None
 
-        # TODO: remove mock
-        # self.image_path = get_output_dir() / 'thumbnails'
-        self.image_path = Path('thumbnails')
-        self.image_path.mkdir(exist_ok=True)
-
-
-    def scrape_news(self, search_phrase: str,
-                    previous_date: date,
-                    section: Literal['all', 'world', 'business', 'legal', 
-                                     'markets', 'breakingviews', 'technology', 
-                                     'sustainability', 'science', 'sports', 'lifestyle', ] = 'all') -> Optional[List[NewsArticle]]:
         
-        print(f'Search Phrase: {search_phrase}')
-        print(f'Section: {section}')
+        # TODO: using another lib, like requests or urllib, because HTTP RPA slow download image
+        # self.http = HTTP()
+    
+    @property
+    def driver(self):
+        if self._driver is None:
+            chrome_options = Options()
+            chrome_options.page_load_strategy = 'eager' 
+            self._driver = uc.Chrome(options=chrome_options)
+        return self._driver 
+
+
+    def scrape_news(self, scrape_id: str, search_phrase: str,
+                    earliest_date: date,
+                    section: str = 'all') -> Optional[List[NewsArticle]]:
+        
+        logging.info(f'Search Phrase: {search_phrase}')
+        logging.info(f'Section: {section}')
 
 
         query =  '+'.join(search_phrase.split())
         offset = self.offset.format(0)
 
         url = self.base_url.format(query, section, offset)
-        self.browser.open_available_browser(url=url, maximized=True, headless=True, options={'capabilities': {"pageLoadStrategy": "eager"}})    
+
+        # exploring captcha breaker
+        browser_alias = 'undetected_chrome'
+        self.browser.register_driver(driver=self.driver, alias=browser_alias)
+        self.browser.switch_browser(browser_alias)
+        self.browser.set_selenium_implicit_wait(timedelta(seconds=50))
+        self.browser.go_to(url=url)
+
+        # exploring explict waits
         self.wait = WebDriverWait(self.browser.driver, 10)
         
 
         try:
+            # exploring selenium wait with selenium RPA
             offset_element = self.wait.until(
                 EC.presence_of_element_located(
                     (By.XPATH, Elements.ALL_OFFSET.value.removeprefix('xpath:'))
@@ -69,24 +87,37 @@ class SeleniumScraper(Scraper):
             )
         except TimeoutException:
             logging.error(f'No search result match the term: {search_phrase}')
+            logging.warning(self.browser.get_source())
             self.browser.close_browser()
             return
         
         max_offset = int(offset_element.text.split()[-1])
         offsets = range(0, max_offset, 20)
 
-        print(f'Offset range: {max_offset//20}')
+        logging.info(f'Offset range: {max_offset//20}')
 
         news_articles = []
         break_scrape = False
 
         for offset in offsets:
+            # exploring wait from RPAframework
+            try:
+                self.browser.wait_until_page_contains_element(f'xpath:{Elements.SEARCH_TITLE.value}', 40)
+            except TimeoutException:
+                logging.error('Lazy page.')
+                return
+
             news_list = self.browser.get_webelements(f'css:{Elements.NEWS_LIST.value}')
+            total_height: int = self.browser.execute_javascript("return document.body.scrollHeight")
+            scroll_height = (total_height*0.8)//len(news_list)
+            scroll_coord = 0
 
             for news in news_list:
+                scroll_coord += scroll_height
                 article_id = str(uuid4())
+                self.browser.execute_javascript(f"window.scrollBy(0, {scroll_coord});")
                 news_title = self.browser.get_webelement(f'css:{Elements.NEWS_TITLE.value}', news)
-                print(f'Collecting: {news_title}')
+                logging.info(f'Collecting: {news_title.text}')
 
                 news_clickable = news.find_element(By.CSS_SELECTOR, Elements.LINK.value)
                 news_url: str = news_clickable.get_attribute('href')
@@ -94,42 +125,47 @@ class SeleniumScraper(Scraper):
                 news_date = '-'.join(news_url_splitted[-3:]).removesuffix('/')
                 news_date = datetime.strptime(news_date, "%Y-%m-%d").date()
 
-                if news_date < previous_date:
+                if news_date < earliest_date:
                     break_scrape = True
                     break
                 
                 try:
-                    article_section = self.browser.get_webelement(f'css:{Elements.NEWS_SECTION.value}', news)
-                except NoSuchElementException:
+                    article_section = self.browser.get_webelement(f'css:{Elements.NEWS_SECTION.value}', news).text
+                except ElementNotFound:
                     article_section = None
 
 
-                print(f'Saving thumbnail: {news_title}')
+                logging.info(f'Saving thumbnail: {news_title}')
+
+                self.image_path = get_output_dir() / f'results_{scrape_id}' / 'thumbnails'
+                self.image_path.mkdir(exist_ok=True, parents=True)
 
                 image_element = news.find_element(By.CSS_SELECTOR, Elements.IMAGE.value)
                 image_url = image_element.get_attribute('src')
                 image_file = self.image_path / article_id
                 image_file = image_file.with_suffix('.png')
                 
-                self.http.download(image_url, image_file)
+                # self.http.download(image_url, image_file)
+
 
                 article = NewsArticle(
                     article_id=article_id,
-                    title=news_title,
+                    title=news_title.text,
                     date=news_date,
                     url=news_url,
                     image_path=str(image_file),
                     extracted_section=article_section,
                     selected_section=section
                 )
-                
-                article.selected_section = section
-                
+                                
                 news_articles.append(article)
             
             if not break_scrape:
+                self.browser.execute_javascript("window.stop();")
+                logging.info(f'Go to Offset: {offset}')
                 self.browser.go_to(self.base_url.format(query, section, offset))
             else:
-                print(f'Finish Scrape in offset: {offset}')
+                logging.info(f'Finish Scrape in offset: {offset}')
         
         self.browser.close_all_browsers()
+        return news_articles
--- src/core/application/scrape_news.py
+++ src/core/application/scrape_news.py
@@ -1,18 +1,60 @@
 from core.domain.interfaces import Scraper
+from core.domain.interfaces import Respository
 from core.domain.news_processor import contains_money, count_phrases
-from datetime import datetime, date
+from datetime import date
 from typing import Tuple
+from uuid import uuid4
+import logging
 
 
 class ScrapeNews:
-    def __init__(self, scraper: Scraper) -> None:
+    def __init__(self, scraper: Scraper, repositoy: Respository):
         self.scraper = scraper
-    
+        self.repository = repositoy
+        self._scrape_id = None
+
+
+    @property
+    def scrape_id(self) -> str:
+        """
+        Generates and returns a unique scrape ID if it has not already been set.
+
+        The scrape ID is a string derived from the first segment of a newly generated UUID.
+        If the scrape ID has already been set (i.e., is not `None`), it simply returns the
+        existing ID.
+
+        Returns:
+            str: A unique scrape ID.
+        """
+        if self._scrape_id is None:
+            self._scrape_id = str(uuid4()).split('-')[0]
+        return self._scrape_id
+
     
     def get_search_months(self, opt: int) -> Tuple[date, date]:
+        """
+        Returns a tuple of two dates representing the first day of the earliest month 
+        and the first day of the latest month based on the specified number of months.
+
+        The function calculates the start date of the first month and the last month in the range
+        of the given number of months. The `opt` parameter specifies how many months back from 
+        the current month should be considered. For example, if `opt` is 3 and the current month 
+        is August, it will return the first day of June and the first day of August.
+
+        Args:
+            opt (int): The number of months to include in the search range. Must be non-negative.
+
+        Returns:
+            Tuple[date, date]: A tuple containing two dates:
+                - The first date is the first day of the earliest month in the range.
+                - The second date is the first day of the latest month in the range.
+                
+        Raises:
+            ValueError: If `opt` is negative.
+        """
         if opt < 0:
             raise ValueError("The number of months must be non-negative.")
-        today: date = datetime.date.today().replace(day=1)
+        today: date = date.today().replace(day=1)
         dates = [today]
         for _ in range(opt - 1):
             if today.month == 1:
@@ -23,18 +65,29 @@ class ScrapeNews:
         dates = dates[::-1]
         return dates[0], dates[-1]
     
-    
-    def scrape(self, search_phrase: str, date_option: int, section: str = None):
-        previous_date, _ = self.get_search_months(date_option)
+
+    def scrape_and_save(self, search_phrase: str, date_option: int, section: str = 'all'):
+        earliest_date, _ = self.get_search_months(date_option)
         
+        logging.info(f'Scrape ID: {self.scrape_id}')
+
         news_list = self.scraper.scrape_news(
+            scrape_id=self.scrape_id,
             search_phrase=search_phrase,
-            previous_date=previous_date,
+            earliest_date=earliest_date,
             section=section
         )
 
+        
+
+        # TODO: implements flow if not exists news
         for article in news_list:
             article.contains_money = contains_money(title=article.title,
                                                     description=article.description)
             article.count_phrases = count_phrases(title=article.title,
                                                     description=article.description)
+        
+        self.repository.save(news_list)
+
+
+    
