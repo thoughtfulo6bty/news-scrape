059f28af8cb91cc5aad667050e21e4f0f6dc8c16
Murilo Simao <murilo.m.simao@gmail.com>
2024-08-26 19:06:33 -0300

    add options and remove stealth

--- src/adapters/scraping/selenium_scraper.py
+++ src/adapters/scraping/selenium_scraper.py
@@ -98,23 +98,14 @@ class SeleniumScraper(Scraper):
             options={
                 'capabilities': {
                     "pageLoadStrategy": "eager",
-                    # "goog:chromeOptions": {
-                    #     "excludeSwitches": ["enable-automation"],
-                    #     "useAutomationExtension": False
-                    # }
+                    "goog:chromeOptions": {
+                        "excludeSwitches": ["enable-automation"],
+                        "useAutomationExtension": False
+                    }
                 }
             }
         )
-        stealth(
-            self.browser.driver,
-            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.105 Safari/537.36',
-            languages=["en-US", "en"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-        )
+        
         # chrome_options = Options()
 
         # chrome_options.page_load_strategy = "eager"
